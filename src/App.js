bbe1697860a9ba022a85361a214bef97e36f0b09
Ilia Koulikov <justian17@gmail.com>
2019-10-08 19:01:42 -0400

    default react-app plus random test text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          I am going to add some code here for a test!
         </p>
         <a
           className="App-link"
